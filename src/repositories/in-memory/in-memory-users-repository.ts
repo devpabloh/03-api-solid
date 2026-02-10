c8813505ab1f9610dcef65ee5e6c1370590abbee
Pablo Henrique <devpabloh@gmail.com>
2026-02-09 23:29:19 -0300

    "feat: Adicionando método findById aos repositórios"

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -4,6 +4,16 @@ import type { UsersRepository } from '../users-repository.js'
 export class InMemoryUsersRepository implements UsersRepository {
   item: User[] = []
 
+  async findById(id: string): Promise<User | null> {
+    const user = this.item.find((item) => item.id === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
   async findByEmail(email: string) {
     const user = this.item.find((item) => item.email === email)
 
