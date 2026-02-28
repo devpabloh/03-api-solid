e8faf435900b60945374098133ebc1d12fa76315
pablohenriqueadm94@gmail.com <pablohenriqueadm94@gmail.com>
2026-02-28 13:56:41 -0300

    "feat: criando testes para os check-in"

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,14 +1,27 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository.js'
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { CheckInUseCase } from './check-in.js'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository.js'
+import { Decimal } from '@prisma/client/runtime/client'
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let sut: CheckInUseCase
 
 describe('Check-in Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
-    sut = new CheckInUseCase(checkInsRepository)
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+    gymsRepository.items.push({
+      id: 'gym-01',
+      title: 'My Gym',
+      description: 'Projeto criando um backend para uma academia',
+      phone: '11999999999',
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    })
 
     vi.useFakeTimers()
   })
@@ -21,6 +34,8 @@ describe('Check-in Use Case', () => {
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
@@ -32,12 +47,16 @@ describe('Check-in Use Case', () => {
     await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
-    expect(() =>
+    await expect(() =>
       sut.execute({
         gymId: 'gym-01',
         userId: 'user-01',
+        userLatitude: 0,
+        userLongitude: 0,
       }),
     ).rejects.toBeInstanceOf(Error)
   })
@@ -48,15 +67,39 @@ describe('Check-in Use Case', () => {
     await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
     vi.setSystemTime(new Date(2026, 0, 21, 8, 0, 0))
 
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'My Gym Two',
+      description: 'My second gym',
+      phone: '11999999999',
+      latitude: new Decimal(-7.9787024),
+      longitude: new Decimal(-34.8293001),
+    })
+
     await expect(() =>
       sut.execute({
-        gymId: 'gym-01',
+        gymId: 'gym-02',
         userId: 'user-01',
+        userLatitude: -7.9105512,
+        userLongitude: -34.8267953,
       }),
-    ).resolves.toBeTruthy()
+    ).rejects.toBeInstanceOf(Error)
   })
 })
